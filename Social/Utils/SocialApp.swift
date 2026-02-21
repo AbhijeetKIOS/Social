77368ec0ac68bd9d7d258a3361d67077d124a94d
AbhijeetKIOS <abhicse123@gmail.com>
2026-02-21 22:32:39 +0530

    Initial Commit

---
+++ Social/Utils/SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialApp.swift
+//  Social
+//
+//  Created by Abhijeet Kumar on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Social/View/Main/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Social
+//
+//  Created by Abhijeet Kumar on 21/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
