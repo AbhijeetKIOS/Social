c93378cabf421ff014a50226fbbaf1a08f0bef7e
AbhijeetKIOS <abhicse123@gmail.com>
2026-02-26 17:04:24 +0530

    search View added again

---
+++ Social/View/Search/SearchBar.swift
@@ -0,0 +1,36 @@
+//
+//  SearchBar.swift
+//  Social
+//
+//  Created by Abhijeet Kumar on 27/02/26.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var text: String
+    var body: some View {
+        HStack{
+            TextField("Search...", text: $text)
+                .padding(8)
+                .padding(.horizontal,24)
+                .background(Color(.secondarySystemBackground))
+                .cornerRadius(8)
+                .overlay(
+                    HStack{
+                        Image(systemName: "magnifyingglass")
+                            .foregroundStyle(.gray)
+                            .frame(
+                                minWidth: 0,
+                                maxWidth: .infinity,
+                                alignment: .leading)
+                            .padding(.leading, 8)
+                    }
+                )
+        }
+    }
+}
+
+#Preview {
+    SearchBar(text: .constant("Search..."))
+}
