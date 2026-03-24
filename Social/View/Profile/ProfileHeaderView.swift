677692890388b223fb2254582184f21c712cee99
AbhijeetKIOS <abhicse123@gmail.com>
2026-03-24 12:37:39 +0530

    apply latest changes , that one not there in branch

--- Social/View/Profile/ProfileHeaderView.swift
+++ Social/View/Profile/ProfileHeaderView.swift
@@ -9,7 +9,56 @@ import SwiftUI
 
 struct ProfileHeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            HStack{
+                Image("abhi")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .clipShape(Circle())
+                    .padding(.leading)
+                Spacer()
+                HStack(spacing: 16){
+                    UserStackView()
+                    UserStackView()
+                    UserStackView()
+                }.padding(.trailing,32)
+            }
+            Text("Abhijeet Kumar")
+                .font(.system(size: 15, weight: .semibold))
+                .padding([.leading, .top])
+            Text("IOS Mobile Appilication Developer")
+                .font(.system(size: 15))
+                .padding(.leading)
+                .padding(.top, 1)
+            HStack{
+                Spacer()
+                Button {
+                } label: {
+                    Text("Edit Profile")
+                        .font(.system(size: 14,weight: .semibold))
+                        .frame(width: 360, height: 32)
+                        .foregroundStyle(.black)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 3)
+                                .stroke(Color.gray, lineWidth: 1)
+                        }
+                }
+                Spacer()
+
+            }.padding(.top)
+        }
+    }
+}
+
+struct UserStackView: View{
+    var body: some View{
+        VStack{
+            Text("2")
+                .font(.system(size: 15, weight: .semibold))
+            Text("Followers")
+                .font(.system(size: 15))
+        }
+        .frame(width: 80, alignment: .center)
     }
 }
 
