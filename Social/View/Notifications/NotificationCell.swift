0d82591a34d422a88cf37350a9ebda821d320d39
AbhijeetKIOS <abhicse123@gmail.com>
2026-03-25 00:09:58 +0530

    1. fixed user cell is not visible name and id 2.create complete notification view and cell 3. fixed profile message button is not visible 4.search app is overlapping so adding incline code

---
+++ Social/View/Notifications/NotificationCell.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationCell.swift
+//  Social
+//
+//  Created by Abhijeet Kumar on 24/03/26.
+//
+
+import SwiftUI
+
+struct NotificationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationCell()
+}
