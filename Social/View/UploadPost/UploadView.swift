c6234e8af416719666ec526f623ae3c6a4e9d183
AbhijeetKIOS <abhicse123@gmail.com>
2026-03-26 13:50:04 +0530

    upload Post UI, with image and text field also image button to upload image

--- Social/View/UploadPost/UploadView.swift
+++ Social/View/UploadPost/UploadView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct UploadView: View {
+    @State var selectedImage: UIImage?
+    @State private var postImage: Image?
+    @State var captionText: String = ""
     var body: some View {
-        Text("UploadView")
+        VStack{
+            if postImage != nil {
+                Button {
+                } label: {
+                    Image(systemName: "photo.circle")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                }
+                
+                
+            }
+            else{
+                HStack(alignment: .top){
+                    Image("abhi")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    TextField("Enter your caption", text: $captionText)
+                }.padding()
+                Button {
+                    
+                } label: {
+                    Text("Post")
+                        .font(Font.system(size: 18, weight: .bold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundStyle(Color.white)
+                }
+                .padding()
+                
+                
+            }
+        }
+        Spacer()
     }
 }
 
